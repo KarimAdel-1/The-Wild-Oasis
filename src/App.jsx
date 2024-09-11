94a717fbde9dfbd6ee61f659bf36f2ff1c3ed4a8
Karim Bonda <karimbonda@outlook.com>
2024-09-12 01:40:48 +0300

    chore: Updated project structure and installed necessary dependencies
    - Installed react-router-dom v6.26.2 for routing functionality.
    - Installed styled-components v6.1.13 for styling components.
    - Organized project files and directories:
      - Added feature-based folder structure.
      - Created reusable UI components under the 'ui' directory.
      - Added hooks, services, and global styles for improved architecture.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,26 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="dashboard" element={<Dashboard />} />
+        <Route path="bookings" element={<Bookings />} />
+        <Route path="users" element={<Users />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="account" element={<Account />} />
+        <Route path="login" element={<Login />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
