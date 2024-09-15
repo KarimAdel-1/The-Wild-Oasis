c540872225cac96424a3a94aed052cf3f37ae9e8
Karim Bonda <karimbonda@outlook.com>
2024-09-15 13:18:54 +0300

    Set up Supabase integration
    - Installed @supabase/supabase-js for database interaction
    - Added ESLint support for process.env by updating ESLint config
    - Created supabase.js file in services folder to configure Supabase client
    - Added piCabins.js to fetch cabins data from Supabase

---
+++ src/services/apiCabins.js
@@ -0,0 +1,12 @@
+import supabase from './supabase';
+
+export async function getCabins() {
+  try {
+    let { data, error } = await supabase.from('cabins').select('*');
+    return data;
+    
+  } catch (error) {
+    console.error(error);
+    throw new Error('Cabins could not be loaded');
+  }
+}
