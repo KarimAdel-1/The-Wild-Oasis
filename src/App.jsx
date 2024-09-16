802424697adc566608d92c6b49073dcf25d15d8a
Karim Bonda <karimbonda@outlook.com>
2024-09-16 13:46:53 +0300

    feat: Integrate React Query for fetching cabin data
    - Install React Query.
    - Remove manual data fetching with useEffect and replace with useQuery.
    - Modify supabase.js to use import.meta instead of process for environment variables.
    - Remove 	ry-catch block from getCabins.
    - Create CabinTable component to display cabins in a grid layout using styled components.
    - Add loading state with a spinner and implement error handling using React Query.
    - Enable caching and re-fetching logic for cabin data.
    - Fix import of date-fns in helper.js.
    - Wrap application with QueryClientProvider and add ReactQueryDevtools in App.jsx.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 
 import GlobalStyles from './styles/GlobalStyles';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import Dashboard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
@@ -12,9 +14,18 @@ import Cabins from './pages/Cabins';
 import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 0,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={true} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -31,7 +42,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
